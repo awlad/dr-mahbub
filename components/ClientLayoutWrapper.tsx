c688c510b4838f92468538097296a21315d45012
awlad <awladliton@gmail.com>
2025-08-10 20:12:09 +0600

    Added first version

--- components/ClientLayoutWrapper.tsx
+++ components/ClientLayoutWrapper.tsx
@@ -2,7 +2,7 @@
 
 'use client'
 
-import { useState }' from 'react'
+import { useState } from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import SectionContainer from '@/components/SectionContainer'
