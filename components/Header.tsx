cd3d84e4ff76c045f5ce11d7927140d910009e36
awlad <awladliton@gmail.com>
2025-08-12 01:15:09 +0600

    Fixed yarn build error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import siteMetadata, { headerNavLinks } from '@/data/siteMetadata'
-import Logo from '@/data/logo.svg'
+import Image from './Image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import LanguageSwitcher from './LanguageSwitcher'
@@ -16,7 +16,12 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image
+              src="/static/images/logo2.svg"
+              alt={siteMetadata.headerTitle}
+              width={100}
+              height={100}
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold text-gray-900 sm:block dark:text-gray-100">
